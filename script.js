8ccf70ef618be6ff138308bc9094bf036cbf2963
DilutedPilk <myawesome4@gmail.com>
2026-05-08 09:11:29 -0400

    Solved it probably

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Part 1 --------------------------------------------->
 
 const radius = 5;
 const PI = 3.1415;
@@ -5,28 +6,31 @@ const area = PI * radius * radius;
 const plant_space = 0.8
 let plants = 20
 
-let weeks = 2
+let weeks = 1;
 
 const max_capacity = area / plant_space
 
 const max_capacity_80 = max_capacity * .80
 const max_capacity_50 = max_capacity * .50
 
-if (weeks === 1){
-    plants = plants * 1;
-    console.log(plants)
-} else if (weeks === 2){
-    plants = plants * 2
-    console.log(plants)
-} else if (weeks === 3){
-    plants = plants * 4
-    console.log(plants)
-}
+const i_need_this_to_work = plants * weeks**2
 
-if (plants > max_capacity_80){
+if (i_need_this_to_work > max_capacity_80){
     console.log("Pruned");
-} else if (plants >= max_capacity_50 && plants <= max_capacity_80) {
+} else if (i_need_this_to_work >= max_capacity_50 && i_need_this_to_work <= max_capacity_80) {
     console.log("Monitored");
 } else {
     console.log("Planted");
 }
+
+// Part 2 --------------------------------------------->
+
+plants = 100;
+weeks = 10;
+
+let plants_capacity = weeks*plant_space
+
+const space_of_all_plants = (plants_capacity*plants) 
+
+console.log(`Each plant within the age of ${weeks} weeks would need ${plants_capacity} amount of space.`)
+console.log(`That means that for ${plants} plants, the raduis would need to be ${space_of_all_plants}.`)
