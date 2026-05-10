accbbdc2f83fb7863753171b22bdf4b58f1f5659
DilutedPilk <myawesome4@gmail.com>
2026-05-09 22:23:27 -0400

    Revamped Part 1 to make more sense.

--- script.js
+++ script.js
@@ -4,37 +4,38 @@ const radius = 5;
 const PI = 3.1415;
 const area = PI * radius * radius;
 const plant_space = 0.8
+let weeks;
 let plants = 20
 
-let weeks = 1;
+let plants_per_week = (plants * weeks) * 2
 
-const max_capacity = area / plant_space
+let plants_capacity = plants * plant_space;
 
-const max_capacity_80 = max_capacity * .80
-const max_capacity_50 = max_capacity * .50
+for (weeks = 1; weeks <= 3; weeks++) {
 
-const i_need_this_to_work = plants * weeks**2
+    if (plants_per_week > area * .8) {
+        console.log("Pruned");
+    } else if (plants_per_week >= area * .5 && plants_per_week <= area * .8) {
+        console.log("Monitored");
+    } else {
+        console.log("Planted");
+    }
 
-if (i_need_this_to_work > max_capacity_80){
-    console.log("Pruned");
-} else if (i_need_this_to_work >= max_capacity_50 && i_need_this_to_work <= max_capacity_80) {
-    console.log("Monitored");
-} else {
-    console.log("Planted");
 }
 
-// Part 2 --------------------------------------------->
 
-plants = 100;
-weeks = 10;
+// // Part 2 --------------------------------------------->
 
-let plants_capacity = weeks*plant_space;
+// plants = 100;
+// weeks = 10;
 
-const space_of_all_plants = plants_capacity*plants;
+// let plants_capacity = weeks * plant_space;
 
-console.log(`Each plant within the age of ${weeks} weeks would need ${plants_capacity} amount of space.`);
-console.log(`That means that for ${plants} plants, the raduis would need to be ${space_of_all_plants}.`);
+// const space_of_all_plants = plants_capacity * plants;
 
-// Part 3 --------------------------------------------->
+// console.log(`Each plant within the age of ${weeks} weeks would need ${plants_capacity} amount of space.`);
+// console.log(`That means that for ${plants} plants, the raduis would need to be ${space_of_all_plants} meters.`);
+
+// // Part 3 --------------------------------------------->
 
 
